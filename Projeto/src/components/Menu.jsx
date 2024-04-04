8a003e0cb393a401f4af355b31bd71c3a1ff6cb2
nogueiiiiiiiiira <karencristininogueira@gmail.com>
2024-04-04 16:48:28 -0300

    dropdown Menu

--- Projeto/src/components/Menu.jsx
+++ Projeto/src/components/Menu.jsx
@@ -5,18 +5,71 @@ export default function Menu(){
     
     return(    
         <div className={style.wrapMenu}>
-            <div className={style.navLinks}>
-                <ul>
-                    <li><a href="/CadastrarBibliotecarios">Bibliotecários</a></li>
-                    <li><a href="/CadastrarLivros">Livros</a></li>
-                    <li><a href="/CadastrarLeitores">Leitores</a></li>
-                    <li><a href="/Emprestimos">Empréstimos</a></li>
-                    <li><a href="/Devolucoes">Devoluções</a></li>
-                    <li><a href="/Multas">Multas</a></li>
-                    <li><a href="/">Sair</a></li>
+            <nav>
+                <ul className={style.gap} >
+                    <li className={style.dropDown}>
+                        <a href="">BIBLIOTECÁRIOS</a>
+                        <div className={style.dropdownMenu} >
+                            <a href="/CadastrarBibliotecarios">Cadastrar Bibliotecários</a>
+                            <a href="">Editar Bibliotecários</a>
+                            <a href="">Excluir Bibliotecários</a>
+                            <a href="">Visualizar Bibliotecários</a>
+                            <br />
+                        </div>
+                    </li>
+                    <li className={style.dropDown}>
+                        <a href="">LIVROS</a>
+                        <div className={style.dropdownMenu} >
+                            <a href="/CadastrarLivros">Cadastrar Livros</a>
+                            <a href="">Editar Livros</a>
+                            <a href="">Excluir Livros</a>
+                            <a href="">Visualizar Livros</a>
+                            <br />
+                        </div>
+                    </li>
+                    <li className={style.dropDown}>
+                        <a href="">LEITORES</a>
+                        <div className={style.dropdownMenu} >
+                            <a href="/CadastrarLeitores">Cadastrar Leitores</a>
+                            <a href="">Editar Leitores</a>
+                            <a href="">Excluir Leitores</a>
+                            <a href="">Visualizar Leitores</a>
+                            <br />
+                        </div>
+                    </li>
+                    <li className={style.dropDown}>
+                        <a href="">EMPRÉSTIMOS</a>
+                        <div className={style.dropdownMenu} >
+                            <a href="/Emprestimos">Efetuar Empréstimo</a>
+                            <a href="">Visualizar Empréstimos</a>
+                            <br />
+                        </div>
+                    </li>
+                    <li className={style.dropDown}>
+                        <a href="">DEVOLUÇÕES</a>
+                        <div className={style.dropdownMenu} >
+                            <a href="/Devolucoes">Efetuar Devoluções</a>
+                            <a href="">Visualizar Devoluções</a>
+                            <br />
+                        </div>
+                    </li>
+                    <li className={style.dropDown}>
+                        <a href="">MULTAS</a>
+                        <div className={style.dropdownMenu} >
+                            <a href="/Multas">Efetuar Multas</a>
+                            <a href="">Visualizar Multas</a>
+                            <br />
+                        </div>
+                    </li>
+                    <li className={style.dropDown}>
+                        <a href="/">AÇÕES</a>
+                        <div className={style.dropdownMenu} >
+                            <a href="/">Sair</a>
+                            <br />
+                        </div>
+                    </li>
                 </ul>
-            </div>
+            </nav>
         </div>
-
     )
 }
