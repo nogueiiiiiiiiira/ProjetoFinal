0ff24eebd5372f6fc5dab3511a6937a5bd98d3c0
nogueiiiiiiiiira <karencristininogueira@gmail.com>
2024-04-11 16:57:47 -0300

    Sign up template

--- Projeto/src/Login/PrimeiroAcesso.jsx
+++ Projeto/src/Login/PrimeiroAcesso.jsx
@@ -1,25 +1,29 @@
 import style from './PrimeiroAcesso.module.css'
 import React from 'react';
 import LoadingButton from '../components/LoandingButton'; 
+import biblio from '../assets/biblio.jpg'
 
 export const PrimeiroAcesso = () => {
     return(
-        <div className={style.container}>
-            <div className={style.wrapBibliotecarios}>
-                <div>
-                    <h2>Cadastrar-se</h2>
-                </div>
-                <input name='nomeBibliotecarioPA' id='nomeBibliotecarioPA' type="text" placeholder='Digite o nome: '/>
-                <input name='sobrenomeBibliotecarioPA' id='sobrenomeBibliotecarioPA' type="text" placeholder='Digite o sobrenome: '/>
-                <input name='cpfBibliotecarioPA' id='cpfBibliotecarioPA' type="text" placeholder='Digite o CPF: '/>
-                <input name='emailBibliotecarioPA' id='emailBibliotecarioPA' type="text" placeholder='Digite o email:'/>
-                <br />
-                <div className={style.wrapButtonBibliotecarios}>
-                    <LoadingButton />
-                    <button><a href="/">Voltar</a></button>
-                </div>
-            </div>
-        </div>
+        <div className={style.body}>
+           <div className={style.container}>
+            <h2>Sign up to Continue</h2>
+            <form className={style.inputs}>
+              <input name='nomeBibliotecarioPA' id='nomeBibliotecarioPA' type="text" placeholder='type your name: '/>
+              <input name='sobrenomeBibliotecarioPA' id='sobrenomeBibliotecarioPA' type="text" placeholder='type your last name: '/>
+              <input name='login' id='login' type="text" placeholder="type your-email@gmail.com: " required />
+              <input name='senha' id='senha' type="password" placeholder="type your password: " required />
+                
+              <br />
+              <br />
+              <input className={style.button} type="submit" value="Sign up" />
+            </form>
+            <br />
+            <p><a className={style.firstAcess} href="/">Go Back</a></p>
+            <img src={biblio} alt="Image" className={style.image}/>
+          </div>
+        
+      </div>
     )
 }
 
